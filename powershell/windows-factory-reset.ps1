4b868c6f5d21b71e396b8cf24cec14ab6b69ee30
Danny Summerlin <danny@summerlin.co>
2024-11-01 13:40:06 +0000

    Create windows-factory-reset.ps1

---
+++ powershell/windows-factory-reset.ps1
@@ -0,0 +1,10 @@
+# Must be run as admin, will totally reset the computer back to factory so be careful
+$namespaceName = "root\cimv2\mdm\dmmap";
+$className = "MDM_RemoteWipe";
+$methodName = "doWipeMethod";
+$session = New-CimSession;
+$params = New-Object Microsoft.Management.Infrastructure.CimMethodParametersCollection;
+$param = [Microsoft.Management.Infrastructure.CimMethodParameter]::Create("param", ", "String", "In");
+  $params.Add($param);
+$instance = Get-CimInstance -Namespace $namespaceName -ClassName $className -Filter "ParentID='./Vendor/MSFT' and InstanceID='RemoteWipe'";
+$session.InvokeMethod($namespaceName, $instance, $methodName, $params);
