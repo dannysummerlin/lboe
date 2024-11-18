8af2572d62ea386bb758ba383b3ce951557d5f28
Danny Summerlin <danny@summerlin.co>
2024-11-18 12:56:51 +0000

    Create convert-flowXml-to-mermaidChart.ps1

---
+++ powershell/convert-flowXml-to-mermaidChart.ps1
@@ -0,0 +1,36 @@
+# todo - handle more step types
+$types = @(
+    @{ name = "actionCalls"; open = "[/"; close = "/]"; color = ""; },
+    @{ name = "decisions"; open = "{{"; close = "}}"; color = ""; },
+    @{ name = "assignments"; open = "[["; close = "]]"; color = ""; },
+    @{ name = "recordLookups"; open = "[("; close = ")]"; color = ""; },
+    @{ name = "screens"; open = "["; close = "]"; color = ""; },
+    @{  name = "loops"; open = "(("; close = "))"; color = ""; },
+    @{ name = "waits"; open = "{"; close = "}"; color = ""; }
+)
+
+ForEach($f in $(gci *.flow)) {
+    $content = (select-xml -Path $f -XPath '/*').Node
+    $flow = $conent
+    $o = ForEach ($t in $types) {
+        ForEach($s in $content.$($t.name)) {
+            echo ($s.name + $t.open + $s.label + $t.close + "`n")
+            "$($s.name) --> $($s.connector.targetReference ?? $s.defaultConnector.targetReference ?? $s.nextValueConnector.targetReference)`n"
+            $s.noMoreValuesConnector ? (echo "$($s.name) --> $($s.noMoreValuesConnector.targetReference)`n") : $null
+            $s.rules | % { $_ ? (echo "$($s.name) --> $($_.connector.targetReference)`n") : $null }
+            $s.waitEvents | % { $_ ? (echo "$($s.name) --> $($_.connector.targetReference)`n") : $null }
+        }
+    }
+# output to Markdown
+@"
+# Description
+
+Type: $($content.processType)
+$($content.description)
+
+``````mermaid
+flowchart
+$o
+``````
+"@ | out-file -filePath "$($f.name -replace "_", " ").md"
+}
