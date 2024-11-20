64161fd2a39335b0e8e0ce7e7977c106984fd678
Danny Summerlin <danny@summerlin.co>
2024-11-20 10:23:37 +0000

    adding style support

--- powershell/convert-flowXml-to-mermaidChart.ps1
+++ powershell/convert-flowXml-to-mermaidChart.ps1
@@ -1,23 +1,33 @@
 $types = @(
-    @{ name = "actionCalls"; open = "[/"; close = "/]"; color = ""; },
-    @{ name = "decisions"; open = "{{"; close = "}}"; color = ""; },
-    @{ name = "assignments"; open = "[["; close = "]]"; color = ""; },
-    @{ name = "recordLookups"; open = "[("; close = ")]"; color = ""; },
-    @{ name = "screens"; open = "["; close = "]"; color = ""; },
-    @{  name = "loops"; open = "(("; close = "))"; color = ""; },
-    @{ name = "waits"; open = "{"; close = "}"; color = ""; }
+    @{ name = "actionCalls"; open = "{{"; close = "}}"; style = "fill:#dac4f7"; },
+    @{ name = "subflows"; open = "{{"; close = "}}"; style = "fill:#d3c0f9"; },
+    @{ name = "decisions"; open = "{"; close = "}"; style = "fill:#ebd2b4"; },
+    @{ name = "assignments"; open = ">"; close = "]"; style = "fill:#d6f6dd"; },
+    @{ name = "recordLookups"; open = "[/"; close = "\]"; style = "fill:#acecf7"; },
+    @{ name = "recordUpdates"; open = "[\"; close = "/]"; style = "fill:#acecf7"; },
+    @{ name = "recordCreates"; open = "[/"; close = "/]"; style = "fill:#acecf7"; },
+    @{ name = "screens"; open = "["; close = "]"; style = "fill:#f4989c"; },
+    @{  name = "loops"; open = "((("; close = ")))"; style = "fill:#f4989c"; },
+    @{ name = "waits"; open = "{"; close = "}"; style = "fill:#f4989c"; }
 )
+
+$styles = "classDef default color:#000`n"
+ForEach ($t in $types) {
+    $styles += "classDef $($t.name) $($t.style)`n"
+}
+
 
 ForEach($f in $(gci *.flow)) {
     $content = (select-xml -Path $f -XPath '/*').Node
     $flow = $conent
     $o = ForEach ($t in $types) {
         ForEach($s in $content.$($t.name)) {
-            echo ($s.name + $t.open + '"`' + $s.label + '`"' + $t.close + "`n")
-            "$($s.name) --> $($s.connector.targetReference ?? $s.defaultConnector.targetReference ?? $s.nextValueConnector.targetReference)`n"
-            $s.noMoreValuesConnector ? (echo "$($s.name) --> $($s.noMoreValuesConnector.targetReference)`n") : $null
-            $s.rules | % { $_ ? (echo "$($s.name) --> $($_.connector.targetReference)`n") : $null }
-            $s.waitEvents | % { $_ ? (echo "$($s.name) --> $($_.connector.targetReference)`n") : $null }
+            echo ($s.name + $t.open + '"`' + $s.label + '`"' + $t.close + ":::$($t.name)`n")
+            $defaultConnector = $s.connector.targetReference ?? $s.defaultConnector.targetReference ?? $s.nextValueConnector.targetReference
+            $defaultConnector ? (echo "$($s.name) ---> $defaultConnector`n") : $null
+            $s.noMoreValuesConnector ? (echo "$($s.name) ---> $($s.noMoreValuesConnector.targetReference)`n") : $null
+            $s.rules | % { $_ ? (echo "$($s.name) -- $($_.label) ---> $($_.connector.targetReference)`n") : $null }
+            $s.waitEvents | % { $_ ? (echo "$($s.name) ---> $($_.connector.targetReference)`n") : $null }
         }
     }
 # output to Markdown
@@ -29,6 +39,7 @@ $($content.description)
 
 ``````mermaid
 flowchart
+$styles
 $o
 ``````
 "@ | out-file -filePath "$($f.name -replace "_", " ").md"
