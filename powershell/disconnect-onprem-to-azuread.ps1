49ddf7647689bfc129b2fdcf79371b373a03e9b5
Danny Summerlin <danny@summerlin.co>
2025-04-30 13:00:44 +0100

    Update disconnect-onprem-to-azuread.ps1

--- powershell/disconnect-onprem-to-azuread.ps1
+++ powershell/disconnect-onprem-to-azuread.ps1
@@ -6,8 +6,11 @@ $emails = @(
 "test@contoso.com",
 "email@contoso.com"
 )
+$TokenSet = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
 $enabled = $emails | ? { (Get-AzureADUser -ObjectId $_).AccountEnabled -eq $True }
 $disabled = $emails | ? { (Get-AzureADUser -objectid $_).AccountEnabled -eq $False }
 
-$disabled | % { Set-AzureADUser -ObjectId $_ -ImmutableId $null }
+$disabled | % { Set-AzureADUser -ObjectId $_ -ImmutableId (
+	[Convert]::ToBase64String((Get-Random -Count 36 -InputObject $TokenSet))
+) }
