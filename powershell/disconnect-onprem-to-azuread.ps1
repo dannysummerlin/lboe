e009ee60c3cb8ce8733c6a59a424133c8ced253a
Danny Summerlin <danny@summerlin.co>
2025-04-30 12:40:40 +0100

    Create disconnect-onprem-to-azuread.ps1

---
+++ powershell/disconnect-onprem-to-azuread.ps1
@@ -0,0 +1,13 @@
+Install-Module AzureAD
+Import-Module AzureAD
+Connect-AzureAD
+
+$emails = @(
+"test@contoso.com",
+"email@contoso.com"
+)
+
+$enabled = $emails | ? { (Get-AzureADUser -ObjectId $_).AccountEnabled -eq $True }
+$disabled = $emails | ? { (Get-AzureADUser -objectid $_).AccountEnabled -eq $False }
+
+$disabled | % { Set-AzureADUser -ObjectId $_ -ImmutableId $null }
