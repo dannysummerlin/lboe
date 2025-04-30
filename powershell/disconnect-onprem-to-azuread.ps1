a411af240f67e7952d66860f8d71bbaaf71ea1ab
Danny Summerlin <danny@summerlin.co>
2025-04-30 13:14:23 +0100

    Update disconnect-onprem-to-azuread.ps1

--- powershell/disconnect-onprem-to-azuread.ps1
+++ powershell/disconnect-onprem-to-azuread.ps1
@@ -7,14 +7,22 @@ $emails = @(
 "email@contoso.com"
 )
 $TokenSet = [Char[]]'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+$syncExcludedLocation = "OU=Excluded from Sync,DC=contoso,dc=com" # an OU or container in AD to move accounts to
 
 $enabled = $emails | ? { (Get-AzureADUser -ObjectId $_).AccountEnabled -eq $True }
 $disabled = $emails | ? { (Get-AzureADUser -objectid $_).AccountEnabled -eq $False }
 
+# save Guids to save from recycling bin
 $disabled_guids = $disabled | % { (Get-AzureADUser -ObjectId $_).ObjectId }
+# set dummy value on ImmutableId
 $disabled | % { Set-AzureADUser -ObjectId $_ -ImmutableId (
 	[Convert]::ToBase64String((Get-Random -Count 36 -InputObject $TokenSet))
 ) }
 
-# after AD sync
-$disabled_guids | % { Restore-AzureADMSDeletedDirectoryObject -Id $_ }
+# move accounts in AD
+$disabled | % { Get-ADuser -filter "userprincipalname -eq '$_'" | Move-ADObject -TargetPath $syncExcludedLocation }
+# force AD-Azure sync
+Start-ADSyncSyncCycle -PolicyType Delta
+Start-Sleep -Seconds 60
+# restore accounts
+$disabled_guid | % { Restore-AzureADMSDeletedDirectoryObject -Id $_ }
