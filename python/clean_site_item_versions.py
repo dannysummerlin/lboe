69ff43d430c2dfcb170599e1893e49b301324e3a
Danny Summerlin <danny@summerlin.co>
2025-05-16 13:17:23 +0100

    Update clean_site_item_versions.py

--- python/clean_site_item_versions.py
+++ python/clean_site_item_versions.py
@@ -10,26 +10,32 @@ def get_file_versions(site_name, page_size = 5000, list_title = "Documents"):
 	app_username = os.environ['SHAREPOINT_USER']
 	app_password = os.environ['SHAREPOINT_PASSWORD']
 	user_credentials = UserCredential(app_username,app_password)
-	base_url = os.environ['SHAREPOINT_BASEURL']
-	context = ClientContext(f'{base_url}{site_name}').with_credentials(user_credentials)
+	sharepoint_prefix = os.environ['SHAREPOINT_PREFIX']
+	url = f'https://{sharepoint_prefix}.sharepoint.com/sites/{site_name}'
+	context = ClientContext(url).with_credentials(user_credentials)
 	library = context.web.default_document_library()
 	items = (
 		library.items
-		.filter("ContentType ne 'Folder'")
-		.select(['versions/versionlabel','versions/iscurrentversion'])
-		.expand(["File","versions"])
+		.select(['Id','FileSystemObjectType','versions/ID','versions/versionlabel','versions/iscurrentversion'])
+		.expand(["File","File/versions"])
 		.get_all(page_size, print_progress).execute_query()
 	)
+	items = [i for i in items if i.file_system_object_type == 0]
 	return items, context
 
 def clean_site_items(site_name, page_size = 5000, list_title = "Documents", keep_at_least = 2,confirmation = False):
 	try:
-		items, context = get_file_versions(site_name, page_size, list_title)
-		if confirmation:
-			for idx, item in enumerate(items):
-				if item.file.major_version > keep_at_least:
-					[i.delete_object().execute_query() for i in item.versions if not i.is_current_version and float(i.version_label) < len(versions) - keep_at_least]
-			return True
+		output = []
+		items, context = get_file_versions(site_name)
+		for item in items:
+			if len(item.file.versions) > versions_to_keep:
+				output.append({
+					'name': item.file.name,
+					'size': item.file.length/1000,
+					'versions': len(item.file.versions),
+				})
+				[item.file.versions.delete_by_label(i.version_label).execute_query() for i in item.file.versions if not i.is_current_version and float(i.version_label) < ( item.file.major_version - versions_to_keep )]
+			return output
 		else:
 			raise('skipping unconfirmed job')
 	except Exception as e:
